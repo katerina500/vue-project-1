b88717330bbcee8dfe319e2891a497d17db79886
katerina500 <ekaterina.paramore@yandex.ru>
2024-05-09 18:48:11 +0400

    +

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <a-layout>
+      <a-layout-sider>Sider</a-layout-sider>
+      <a-layout>
+      <a-layout-header>
+        <a-button type="primary" href="/">Home</a-button>
+        <a-button type="primary" href="/about">About</a-button>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </a-layout-header>
+        <a-layout-content><RouterView /></a-layout-content>
+        <a-layout-footer>Footer</a-layout-footer>
+      </a-layout>
+    </a-layout>
+    <template>
+  <a-menu v-model:selectedKeys="current" mode="horizontal" :items="items" />
+</template>
+
+
+
+</template>
+
+
+
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,46 @@
+<script>
+export default {
+ props: ['msg'],
+
+ data() {
+    return {
+      timerId: -1,
+      currentTextIndex: 0,
+      text: 'You’ve successfully created a project with',
+      greetingTexts:[
+      'You’ve successfully created a project',
+      'Vue’s official documentation provides you with all information you need to get started.',
+      'Got stuck? Ask your question on Vue Land, our official Discord server, or StackOverflow.',
+      'As an independent project, Vue relies on community backing for its sustainability.',
+      ]
+    }
+  },
+  mounted() {
+    this.text = this.greetingTexts[0];
+    this.timerId = setTimeout(this.changeText, 2000);
+  },
+  unmounted() {
+    clearTimeout(this.timerId);
+  },
+  methods:{
+    changeText() {
+      clearTimeout(this.timerId);
+      this.currentTextIndex++;
+      if(this.currentTextIndex == this.greetingTexts.length) this.currentTextIndex = 0;
+      this.text = this.greetingTexts(this.currentTextIndex);
+      this.timerId = setTimeout(this.changeText, 2000);
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="greetings">
+    <h1 class="green" @click="changeText">{{ msg }}</h1>
+    <h3>
+      {{ timerId }}
+      {{ text }}
+    </h3>
+  </div>
+</template>
+
